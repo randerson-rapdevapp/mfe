c245ac1b01c9db836c3020e6148861c9c687a528
R. Anderson <randerson@legalintake.com>
2025-09-04 16:31:54 -0500

    Added container deploy script.

--- packages/marketing/config/webpack.dev.js
+++ packages/marketing/config/webpack.dev.js
@@ -1,6 +1,5 @@
 const { merge } = require("webpack-merge");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const packageJson = require("../package.json");
 
 const commonConfig = require("./webpack.common");
@@ -20,9 +19,6 @@ const devConfig = {
         "./MarketingApp": "./src/bootstrap",
       },
       shared: packageJson.dependencies,
-    }),
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
     }),
   ],
 };
